1b78b2eb7dfa95d48f35a1af8e22ad98d8c222f8
satish-sunkara <satishsunkara01@gmail.com>
2023-12-14 16:43:41 +0530

    roboshop mongodb change1

--- mongdb.sh
+++ mongdb.sh
@@ -1,3 +1,18 @@
 #!?bin/bash
 
-echo "Hi"
+USERID=$(id -u)
+DATE=$(DATE +%F_%H:%M:%S)
+
+LOG="/tmp/$0-$DATE.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$Y Please login as root user $N"
+    exit 1
+fi
+
+
