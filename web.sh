85ee774cfd6a96fb2d99aa4093c924a0d3a813e5
satish-sunkara <satishsunkara01@gmail.com>
2023-12-17 09:12:27 +0530

    app update1

--- web.sh
+++ web.sh
@@ -15,7 +15,7 @@ VALIDATE(){
         echo -e " $2 ....... $R FAILED $N"
         exit 1
     else
-        echo " $2 ....... $G SUCCESS $N"
+        echo -e " $2 ....... $G SUCCESS $N"
     fi
 }
 
