dbcc4f33ef51020f0b2974600f776fd73e05d281
satish-sunkara <satishsunkara01@gmail.com>
2023-12-17 09:31:06 +0530

    app update1

--- redis.sh
+++ redis.sh
@@ -11,15 +11,15 @@ N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo " $2 ...... $R FAILED $N"
+        echo -e " $2 ...... $R FAILED $N"
         exit 1
     else
-        echo " $2 ...... $G SUCCESS $N"
+        echo -e " $2 ...... $G SUCCESS $N"
     fi
 }
 
 if [ $USERID -ne 0 ]; then
-    echo "$R Please login as a root user $N"
+    echo -e "$R Please login as a root user $N"
     exit 1
 else
     VALIDATE $USERID "Logging as a root"
